1f94c28a8bea942c47a0ee70eb5984c0d2b6119f
TonyFreddy <kounleadissa@gmail.com>
2024-08-07 16:59:05 +0200

    fchange: delete console in server.js

--- src/server.js
+++ src/server.js
@@ -27,7 +27,6 @@ app.get("/", (req, res) => {
 
 sequelize.sync()
     .then(() => {
-        console.log('Database synchronized'); 
 
         app.listen(PORT, () => {
             console.log(`Server is running on port: ${PORT}`);

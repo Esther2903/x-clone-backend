b35f84f32c3415e802c5377f7004cb3ca9f20c77
TonyFreddy <kounleadissa@gmail.com>
2024-08-07 17:31:13 +0200

    change: added the function of retrieve all the users

--- src/feature/users/userController.js
+++ src/feature/users/userController.js
@@ -2,7 +2,6 @@ const userService = require("./userService");
 
 
 exports.signup = async (req, res) => {
-    console.log('Signup Request Body:', req.body);
     try {
         const user = await userService.createUser(req.body);
         res.status(201).json({ id: user.id, email: user.email });
@@ -10,3 +9,33 @@ exports.signup = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 };
+
+
+/*exports.login = async (req , res) => {
+    try {
+        const {email , password} = req.body;
+        const user = await userService.loginUser(email, password);
+        res.json({ id: user.id, email: user.email });
+    } catch (error) {
+        res.status(401).json({error: error.message});
+    }
+};*/
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+/*exports.getUser = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};*/
--- src/feature/users/userService.js
+++ src/feature/users/userService.js
@@ -1,5 +1,4 @@
 const User  = require('./userModel');
-console.log('User Model:', User);
 const bcrypt = require ("bcrypt");
 
 exports.createUser = async (userData) => {
@@ -8,3 +7,25 @@ exports.createUser = async (userData) => {
     const user = await User.create({ ...userData, password: hashedPassword });
     return user;
 };
+
+exports.getAllUsers = async () => {
+    return User.findAll();
+}
+/*
+exports.loginUser = async (email, password) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new Error('User not found');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error('Invalid credentials');
+
+    return user;
+};
+
+
+
+
+exports.getUserById = async (id) => {
+    return User.findByPk(id);
+};
+*/

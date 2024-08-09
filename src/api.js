8c6f4f84abd5f48d7f5ed9dced8a69c0e8d19f15
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-08-09 16:39:13 +0100

    faet: pull the tony branch

--- src/api.js
+++ src/api.js
@@ -4,9 +4,7 @@ const authRoutes = require('./feature/auth/authRoutes');
 const followershipRoute = require('./feature/followerships/followershipRoute');
 const tweetRoute = require('./feature/tweets/tweetRouter');
 const mentionRoute = require('./feature/mentions/mentionRoute');
-
 const listRoute = require('./feature/lists/listRoute');
-
 const messageRoute = require('./feature/messages/messageRouter');
 const likeRoute = require('./feature/likes/likeRouter');
 const communityRoute = require('./feature/community/communityRouter');
@@ -21,13 +19,10 @@ router.use('/followerships', followershipRoute);
 router.use('/tweets', tweetRoute);
 router.use('/retweets' , retweetRoute);
 router.use('/mentions', mentionRoute);
-
 router.use('/lists', listRoute);
-
 router.use('/messages', messageRoute);
 router.use('/likes', likeRoute);
 router.use('/community', communityRoute);
-
 router.use('/hastags', hashtagRoute);
 
 module.exports = router; 

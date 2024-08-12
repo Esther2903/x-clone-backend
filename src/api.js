e01c896ec9ec2a9929e61a2039b86bb824dc2fd7
TonyFreddy <kounleadissa@gmail.com>
2024-08-12 12:48:21 +0200

    bug fixe conflit

--- src/api.js
+++ src/api.js
@@ -9,15 +9,15 @@ const messageRoute = require('./feature/messages/messageRouter');
 const likeRoute = require('./feature/likes/likeRouter');
 const communityRoute = require('./feature/community/communityRouter');
 const retweetRoute = require('./feature/retweets/retweetRoute');
-<<<<<<< HEAD
+
 const blockRoute = require('./feature/block/blockRoute');
 const muteRoute = require('./feature/mutes/muteRoute');
 //const bookmarkRoute = require('./feature/bookmark/bookmarkController');
 const spaceRoute = require('./feature/spaces/spaceRoute');
 const space_participantRoute = require('./feature/space_participants/space_participantRoute');
-=======
+
 const hashtagRoute = require('./feature/hashtag/hashtagRouter');
->>>>>>> 734e1db1a39121a66b83ac37257bedd9d84821b5
+
 
 const router = express();
 
@@ -27,7 +27,7 @@ router.use('/followerships', followershipRoute);
 router.use('/tweets', tweetRoute);
 router.use('/retweets' , retweetRoute);
 router.use('/mentions', mentionRoute);
-<<<<<<< HEAD
+
 router.use('/block', blockRoute);
 router.use('/lists', listRoute);
 router.use('/mutes', muteRoute);
@@ -35,9 +35,8 @@ router.use('/mutes', muteRoute);
 router.use('/spaces' , spaceRoute);
 router.use('/space_participants' , space_participantRoute);
 
-=======
+
 router.use('/lists', listRoute);
->>>>>>> 734e1db1a39121a66b83ac37257bedd9d84821b5
 router.use('/messages', messageRoute);
 router.use('/likes', likeRoute);
 router.use('/community', communityRoute);

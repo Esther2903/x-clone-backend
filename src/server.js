6362f7b9edca1545c20ea153cbe5b2b9c045f91f
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-08-12 17:58:58 +0100

    faet: add HTTPS support with helmet security enhancements

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const errorHandler = require('./middlewares/errorHandler'); 
 const sequelize = require("./config/db_config"); 
 const router = require("./api"); 
@@ -9,13 +14,42 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const privateKey = fs.readFileSync('./server.key', 'utf-8');
+const certificate = fs.readFileSync('./server.cert', 'utf-8');
+const credentials = { key: privateKey, cert: certificate};
 
+app.use(helmet());
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
+app.use(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: false,
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "https://example.com"],
+          styleSrc: ["'self'", "https://example.com"],
+          imgSrc: ["'self'", "https://example.com"],
+          objectSrc: ["'none'"],
+          upgradeInsecureRequests: [],
+        },
+      },
+      xFrameOptions: { action: "deny" },
+    }),
+);
+
+app.use(helmet.xXssProtection())
+
+app.use((req, res, next) => {
+    if(req.secure){
+        return next();
+    }
+    res.redirect(`https://${req.headers.host}${req.url}`)
+})
 
 app.use('/api', router); 
-
 
 app.use(errorHandler);
 
@@ -27,9 +61,18 @@ app.get("/", (req, res) => {
 
 sequelize.sync()
     .then(() => {
+        const httpsServer = https.createServer(credentials, app);
+        httpsServer.listen(PORT, () => {
+            console.log(`Server is running securely on https://localhost:${PORT}`);
+        });
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port: ${PORT}`);
+        const httpApp = express();
+        httpApp.use((req, res, next) => {
+            res.redirect(`https://${req.headers.host}${req.url}`);
+        });
+        const httpServer = http.createServer(httpApp);
+        httpServer.listen(80, () => {
+            console.log(`HTTP Server running on port 80 and redirecting to HTTPS`);
         });
     })
     .catch(error => {
